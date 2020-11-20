ba182f9fee728dadef436d7d3c9f171ae1dcd10c
Marcus Almert <marcus.almert@gmail.com>
2020-11-20 16:38:19 +0100

    CameraNode added

---
+++ framework/include/CameraNode.hpp
@@ -0,0 +1,14 @@
+//
+// Created by marcus on 20.11.20.
+//
+
+#ifndef OPENGL_FRAMEWORK_CAMERANODE_HPP
+#define OPENGL_FRAMEWORK_CAMERANODE_HPP
+
+
+class CameraNode {
+
+};
+
+
+#endif //OPENGL_FRAMEWORK_CAMERANODE_HPP
---
+++ framework/source/CameraNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marcus on 20.11.20.
+//
+
+#include "CameraNode.hpp"
