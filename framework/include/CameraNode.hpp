60de9e1719cf08fdb49c0748f466054c23b37bb5
Marcus Almert <marcus.almert@gmail.com>
2020-11-20 16:56:53 +0100

    CameraNode hpp

--- framework/include/CameraNode.hpp
+++ framework/include/CameraNode.hpp
@@ -1,13 +1,31 @@
-//
-// Created by marcus on 20.11.20.
-//
-
 #ifndef OPENGL_FRAMEWORK_CAMERANODE_HPP
 #define OPENGL_FRAMEWORK_CAMERANODE_HPP
 
+#include "Node.hpp"
+#include "glm/glm.hpp"
 
-class CameraNode {
+class CameraNode : public Node {
+private:
+    bool isPerspective_;
+    bool isEnabled_;
+    glm::mat4 projectionMatrix_;
 
+public:
+    CameraNode();
+
+    CameraNode(bool isPerspective, bool isEnabled);
+
+    CameraNode(bool isPerspective, bool isEnabled, glm::mat4 const &projectionMatrix);
+
+    bool getPerspective();
+
+    bool getEnabled();
+
+    void setEnabled(bool);
+
+    glm::mat4 getProjectionMatrix();
+
+    void setProjectionMatrix(glm::mat4 const &mat);
 };
 
 
--- framework/source/CameraNode.cpp
+++ framework/source/CameraNode.cpp
@@ -1,5 +1,1 @@
-//
-// Created by marcus on 20.11.20.
-//
-
 #include "CameraNode.hpp"
