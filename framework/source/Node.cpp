ec2087a26fa7ddbe67cae557bafeb09f6c6361db
nire2864 <luigi.vinzenz.portwich@uni-weimar.de>
2020-11-21 17:26:43 +0100

    [ADD] getter/ setter

--- framework/source/Node.cpp
+++ framework/source/Node.cpp
@@ -25,6 +25,97 @@ Node::Node(std::string name, const std::shared_ptr<Node> &parent) :
 
 Node::~Node() = default;
 
+//setter
+
+void Node::setParent(const std::shared_ptr<Node> &parent) {
+    parent_ = parent;
+    depth_ = parent_->getDepth() + 1;
+    path_ = parent->getPath() + "/" + name_;
+}
+
+void Node::setLocalTransform(const glm::mat4 &mat) {
+    localTransform_ = mat;
+}
+
+void Node::setWorldTransform(const glm::mat4 &mat) {
+    worldTransform_ = mat;
+}
+
+void Node::addChildren(const std::shared_ptr<Node> &node) {
+    children_.push_back(node);
+}
+
+std::shared_ptr<Node> Node::removeChildren(const std::string &name) {
+    // search for child recursively
+    auto found_child = this->getChildren(name);
+    // if child was found
+    if (found_child != nullptr) {
+        // get parent to access childrenList
+        auto parent_found_child = found_child->getParent();
+        // if parent was found
+        if (parent_found_child != nullptr)
+            // remove child from the parents childrenList
+            parent_found_child->children_.remove(found_child);
+    }
+    return found_child;
+}
+
+void Node::setDistance(float distance) {
+    distance_ = distance;
+}
+
+void Node::setSpeed(float speed) {
+    speed_ = speed;
+}
+
+void Node::setSize(float size) {
+    size_ = size;
+}
+
+
+// getter
+
+std::list<std::shared_ptr<Node>> Node::getChildrenList() {
+    return children_;
+}
+
+glm::mat4 Node::getLocalTransform() {
+    return localTransform_;
+}
+
+glm::mat4 Node::getWorldTransform() {
+    return worldTransform_;
+}
+
+float Node::getSpeed() const {
+    return speed_;
+}
+
+float Node::getDistance() const {
+    return distance_;
+}
+
+float Node::getSize() const {
+    return size_;
+}
+
+std::shared_ptr<Node> Node::getChildren(std::string const &name) {
+    // iterate over childrenList
+    for (auto child : children_) {
+        // if direct child
+        if (child->name_ == name)
+            return child;
+        else {
+            // search recursively
+            std::shared_ptr<Node> next_child = child->getChildren(name);
+            if (next_child != nullptr)
+                return child->getChildren(name);
+        }
+    }
+    // nothing found :(
+    return nullptr;
+}
+
 std::string Node::getName() {
     return name_;
 }
@@ -51,78 +142,18 @@ std::string Node::getPath() {
     }
 }
 
-void Node::setParent(const std::shared_ptr<Node> &parent) {
-    parent_ = parent;
-    depth_ = parent_->getDepth() + 1;
-    path_ = parent->getPath() + "/" + name_;
-}
-
 std::shared_ptr<Node> Node::getParent() {
     return parent_;
 }
 
-void Node::addChildren(const std::shared_ptr<Node> &node) {
-    children_.push_back(node);
-}
-
-std::shared_ptr<Node> Node::removeChildren(const std::string &name) {
-    // search for child recursively
-    auto found_child = this->getChildren(name);
-    // if child was found
-    if (found_child != nullptr) {
-        // get parent to access childrenList
-        auto parent_found_child = found_child->getParent();
-        // if parent was found
-        if (parent_found_child != nullptr)
-            // remove child from the parents childrenList
-            parent_found_child->children_.remove(found_child);
-    }
-    return found_child;
-}
-
-std::shared_ptr<Node> Node::getChildren(std::string const &name) {
-    // iterate over childrenList
-    for (auto child : children_) {
-        // if direct child
-        if (child->name_ == name)
-            return child;
-        else {
-            // search recursively
-            std::shared_ptr<Node> next_child = child->getChildren(name);
-            if (next_child != nullptr)
-                return child->getChildren(name);
-        }
-    }
-    // nothing found :(
-    return nullptr;
-}
+// functions
 
 void Node::printChildren(int level) {
     if (level == 0)
         std::cout << "." << "- " << this->getName() << std::endl;
     else
         std::cout << std::string(level, '|') << "- " << this->getName() << std::endl;
-    for (auto child: children_) {
+    for (const auto& child: children_) {
         child->printChildren(level + 1);
     }
-}
-
-std::list<std::shared_ptr<Node>> Node::getChildrenList() {
-    return children_;
-}
-
-void Node::setLocalTransform(const glm::mat4 &mat) {
-    localTransform_ = mat;
-}
-
-glm::mat4 Node::getLocalTransform() {
-    return localTransform_;
-}
-
-void Node::setWorldTransform(const glm::mat4 &mat) {
-    worldTransform_ = mat;
-}
-
-glm::mat4 Node::getWorldTransform() {
-    return worldTransform_;
 }
